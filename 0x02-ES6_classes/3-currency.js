e550c1ff17ad7da8ff2df221fd387961801ec083
Mwmuraguri <maryannewanjiku87@gmail.com>
2024-01-04 12:27:48 +0000

    classes

---
+++ 0x02-ES6_classes/3-currency.js
@@ -0,0 +1,32 @@
+export default class Currency {
+  constructor(code, name) {
+    if (typeof name === 'string' && typeof code === 'string') {
+      this._code = code;
+      this._name = name;
+    }
+  }
+
+  // get code
+  get code() {
+    return this._code;
+  }
+
+  // set code
+  set code(newCode) {
+    this._code = newCode;
+  }
+
+  // get name
+  get name() {
+    return this._name;
+  }
+
+  // set name
+  set name(newName) {
+    this._name = newName;
+  }
+
+  displayFullCurrency() {
+    return `${this._name} (${this._code})`;
+  }
+}
